1440872bf6d94a96ddff6c9aa2875f1d1c488911
Rafael Lima <rlima@thoughtworks.com>
2014-02-15 16:18:05 -0300

    adding ruby basic

---
+++ basic_examples/ruby_basic.rb
@@ -0,0 +1,40 @@
+puts "Rafael".class
+puts 1.class
+
+nome = :rafael
+puts nome.class
+nome = 1.1
+puts nome.class
+
+puts [1,2,3].class
+
+nome = "Rafael"
+
+puts nome.reverse
+puts nome.downcase
+puts nome.upcase
+puts nome.upcase.capitalize
+nome.reverse!
+puts nome
+
+
+#loop
+
+10.times { |x| puts x }
+
+(23..32).each { |x| puts x }
+
+
+#Symbol vs String
+
+10.times do |x| 
+	puts "String".object_id
+end
+
+
+10.times do |x| 
+	puts :string.object_id
+end
+
+
+
