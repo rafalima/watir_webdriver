0a9cac800e4fa352cf2a63a7f1231509336e1c3c
Rafael Lima <rlima@thoughtworks.com>
2014-02-17 14:02:46 -0300

    moving some basic examples

--- watir.rb
+++
@@ -1,9 +0,0 @@
-require 'watir-webdriver'
-
-browser = Watir::Browser.new :firefox
-
-browser.goto "http://demo.moodle.net/login/index.php"
-browser.text_field(id: 'username').set 'admin'
-browser.text_field(id: 'password').set 'sandbox'
-browser.button(id: 'loginbtn').click
-puts browser.p(id: 'frontpagesettings').exists? && browser.span(xpath: "//span[text()='Site administration']").exists?
