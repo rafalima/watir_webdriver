7ef66c6b25a380be658f6a0f40ce4ad6b81ba230
Rafael <rafalima07@gmail.com>
2014-06-14 19:30:03 -0300

    adding files for local env

--- active_admin_tests/features/support/env.rb
+++ active_admin_tests/features/support/env.rb
@@ -3,7 +3,12 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../', 'lib'))
 require 'rspec-expectations'
 require 'page-object'
 require 'require_all'
+require 'fig_newton'
+
+FigNewton.yml_directory = 'environments'
+FigNewton.load('local.yml')
 
 require_all 'lib'
+
 
 World(PageObject::PageFactory)
