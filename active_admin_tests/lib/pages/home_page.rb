c1894361d6325c2d883aa81c7fce358deeabd2d7
Rafael <rafalima07@gmail.com>
2014-06-14 19:43:45 -0300

    updating home page object

--- active_admin_tests/lib/pages/home_page.rb
+++ active_admin_tests/lib/pages/home_page.rb
@@ -4,7 +4,6 @@ class HomePage
   page_url FigNewton.base_url
 
   link(:sign_up_link, link:"Sign up")
-  div(:flash_notice, id:'flash_notice')
 
   def sign_up
     sign_up_link_element.when_present.click
@@ -13,9 +12,10 @@ class HomePage
 
 
   def successful_signup_message_displayed?(message)
-    wait_until do
-      flash_notice_element.visible?
+    wait_until(5) do
+      text.include? message
     end
+    true
   end
 
 end
